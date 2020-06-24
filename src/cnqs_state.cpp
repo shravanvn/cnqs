8aebf56afe0e1d16f803f96c1b93a33f2f19c424
Saibal De <saibal.de.bnk@gmail.com>
2020-06-24 16:10:13 -0400

    Replace datasets when saving to HDF5 files

--- src/cnqs_state.cpp
+++ src/cnqs_state.cpp
@@ -3,7 +3,7 @@
 void CnqsState::save(const std::string &file_name, unsigned long id) {
     std::string data_set_name = "state_" + std::to_string(id);
     vec_.save(
-        arma::hdf5_name(file_name, data_set_name, arma::hdf5_opts::append));
+        arma::hdf5_name(file_name, data_set_name, arma::hdf5_opts::replace));
 }
 
 void CnqsState::load(const std::string &file_name, unsigned long id) {
