56365e70511a12e60010104193b548515a4c54d1
Saibal De <saibal.de.bnk@gmail.com>
2020-06-27 16:01:00 -0400

    Add CnqsTrivialPreconditioner

---
+++ include/CnqsTrivialPreconditioner.hpp
@@ -0,0 +1,21 @@
+#ifndef CNQS_TRIVIAL_PRECONDITIONER_HPP
+#define CNQS_TRIVIAL_PRECONDITIONER_HPP
+
+#include <string>
+
+#include "CnqsPreconditioner.hpp"
+#include "CnqsVector.hpp"
+
+class CnqsTrivialPrecondtioner : public CnqsPreconditioner {
+public:
+    CnqsTrivialPrecondtioner()
+        : CnqsPreconditioner("cnqs trivial preconditioner") {}
+
+    ~CnqsTrivialPrecondtioner() = default;
+
+    void Solve(const CnqsVector &input_state, CnqsVector &output_state) const {
+        output_state = input_state;
+    }
+};
+
+#endif
