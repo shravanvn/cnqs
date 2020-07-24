47d0ce006d81e9018002c0d1ea86ccf7d6903373
Saibal De <saibal.de.bnk@gmail.com>
2020-07-24 04:11:20 -0400

    Add two examples

---
+++ example/FourierProblem.cpp
@@ -0,0 +1,90 @@
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+
+#include <Teuchos_Comm.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Tpetra_Core.hpp>
+
+#include "Cnqs_FourierProblem.hpp"
+#include "Cnqs_Network.hpp"
+
+void printHelp(const std::string &programName) {
+    std::cout << "USAGE:" << std::endl
+              << "    " << programName
+              << " <networkFileName> <maxFreq> [<maxPowerIter> "
+                 "<tolPowerIter> <maxCgIter> <tolCgIter> [<fileName>]]"
+              << std::endl;
+    std::cout << "DEFAULTS:" << std::endl
+              << "    maxPowerIter : 100" << std::endl
+              << "    tolPowerIter : 1.0e-06" << std::endl
+              << "    maxCgIter    : 1000" << std::endl
+              << "    tolCgIter    : 1.0e-06" << std::endl
+              << "    fileName     : \"\"" << std::endl;
+}
+
+int main(int argc, char **argv) {
+    Tpetra::ScopeGuard tpetraScope(&argc, &argv);
+    {
+        const auto comm = Tpetra::getDefaultComm();
+
+        // parse command line arguments
+        if ((argc == 2) && (std::string(argv[1]).compare("-h") == 0)) {
+            if (comm->getRank() == 0) {
+                printHelp(argv[0]);
+            }
+        } else if ((argc < 3) || (argc > 3 && argc < 7) || (argc > 8)) {
+            if (comm->getRank() == 0) {
+                std::cout << "ERROR: Incorrect number of command line arguments"
+                          << std::endl
+                          << "       Run \"" << argv[0] << " -h\" for help"
+                          << std::endl;
+            }
+        } else {
+            // argc = 3, 7 or 8
+
+            auto network = std::make_shared<Cnqs::Network>(argv[1]);
+            int maxFreq = std::atoi(argv[2]);
+            int maxPowerIter = 100;
+            double tolPowerIter = 1.0e-06;
+            int maxCgIter = 1000;
+            double tolCgIter = 1.0e-06;
+            std::string fileName("");
+
+            if (argc > 3) {
+                maxPowerIter = std::atoi(argv[3]);
+                tolPowerIter = std::atof(argv[4]);
+                maxCgIter = std::atoi(argv[5]);
+                tolCgIter = std::atof(argv[6]);
+            }
+
+            if (argc > 7) {
+                fileName = std::string(argv[7]);
+            }
+
+            // output full command for sanity check
+            if (comm->getRank() == 0) {
+                std::cout << "==main== Begin run configuration" << std::endl;
+                std::cout << "network: " << network->description() << std::endl;
+                std::cout << "max_freq: " << maxFreq << std::endl;
+                std::cout << "max_power_iter: " << maxPowerIter << std::endl;
+                std::cout << "tol_power_iter: " << tolPowerIter << std::endl;
+                std::cout << "max_cg_iter: " << maxCgIter << std::endl;
+                std::cout << "tol_cg_iter: " << tolCgIter << std::endl;
+                std::cout << "file_name: " << fileName << std::endl;
+                std::cout << "==main== End run configuration" << std::endl;
+            }
+
+            // create Fourier problem
+            Cnqs::FourierProblem problem(network, maxFreq, comm);
+
+            // solve Fourier problem
+            problem.runInversePowerIteration(maxPowerIter, tolPowerIter,
+                                             maxCgIter, tolCgIter, fileName);
+        }
+    }
+
+    return 0;
+}
