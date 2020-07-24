cfd9c4f0d3ba581af86c7940320f696f59bcc822
Saibal De <saibal.de.bnk@gmail.com>
2020-07-23 21:18:46 -0400

    Trash old version 0.1.0 code

--- extern/iml/README.md
+++
@@ -1,4 +0,0 @@
-# IML++
-
-This directory contains the iterative methods libary IML++ (version 1.2a)
-available from [https://math.nist.gov/iml++/](https://math.nist.gov/iml++/).
--- src/CnqsPreconditioner.cpp
+++
@@ -1,7 +0,0 @@
-#include "CnqsPreconditioner.hpp"
-
-std::ostream &operator<<(std::ostream &os,
-                         const CnqsPreconditioner &cnqs_preconditioner) {
-    os << cnqs_preconditioner.Describe() << std::flush;
-    return os;
-}
